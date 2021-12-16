98fb726e85d6f8d38f3b44ff3b062213b757aa95
Andre Leoni <andreluizleoni@gmail.com>
2021-12-16 15:33:09 -0200

    add sprites and 3 levels

--- main.lua
+++ main.lua
@@ -1,9 +1,16 @@
 function love.load()
+  love.window.setMode(1000,768)
   wf = require 'libraries/windfield/windfield'
   anim8 = require 'libraries/anim8/anim8'
 
+  sti = require 'libraries/Simple-Tiled-Implementation/sti'
+  cameraFile = require 'libraries/hump/camera'
+
+  cam = cameraFile()
+
   sprites = {}
   sprites.playerSheet = love.graphics.newImage('sprites/playerSheet.png')
+  sprites.enemySheet = love.graphics.newImage('sprites/enemySheet.png')
 
   local grid = anim8.newGrid(
     614,
@@ -12,10 +19,13 @@ function love.load()
     sprites.playerSheet:getHeight()
   )
 
+  local enemyGrid = anim8.newGrid(100, 79, sprites.enemySheet:getWidth(), sprites.enemySheet:getHeight())
+
   animations = {}
   animations.idle = anim8.newAnimation(grid('1-15', 1), 0.05)
   animations.jump = anim8.newAnimation(grid('1-7', 2), 0.05)
   animations.run = anim8.newAnimation(grid('1-15', 3), 0.05)
+  animations.enemy = anim8.newAnimation(enemyGrid('1-2', 1), 0.03)
 
   world = wf.newWorld(0, 800, false)
   world:setQueryDebugDrawing(true)
@@ -25,24 +35,53 @@ function love.load()
   world:addCollisionClass('Danger')
 
   require('player')
-
-  platform = world:newRectangleCollider(250, 400, 300, 100, { collision_class = 'Platform' })
-  platform:setType('static')
+  require('enemy')
 
   dangerZone = world:newRectangleCollider(0, 550, 800, 50, { collision_class = 'Danger' })
   dangerZone:setType('static')
+
+  platforms = {}
+
+  flagX = 0
+  flagY = 0
+
+  currentLevel = "level1"
+
+  loadMap(currentLevel)
 end
 
 function love.update(dt)
   world:update(dt)
 
+  gameMap:update(dt)
+
   playerUpdate(dt)
+  updateEnemies(dt)
+
+  local px, py = player:getPosition()
+  cam:lookAt(px, love.graphics.getHeight()/2)
+
+  local colliders = world:queryCircleArea(flagX, flagY, 10, {'Player'})
+  if #colliders > 0 then
+    if currentLevel == "level1" then
+      loadMap("level2")
+    elseif currentLevel == "level2" then
+      loadMap("level3")
+    end
+  end
 end
 
 function love.draw()
-  world:draw()
+  cam:attach()
 
-  drawPlayer()
+    gameMap:drawLayer(gameMap.layers["Camada de Tiles 1"])
+
+    drawPlayer()
+    drawEnemies()
+
+    world:draw()
+
+  cam:detach()
 end
 
 function love.keypressed(key)
@@ -53,12 +92,53 @@ function love.keypressed(key)
   end
 end
 
-function love.mousepressed(x, y, button)
-  if button == 1 then
-    local colliders = world:queryCircleArea(x, y, 200, {'Platform', 'Danger'})
+function loadMap(mapName)
+  currentLevel = mapName
 
-    for i,c in ipairs(colliders) do
-      c:destroy()
+  destroyAll()
+  player:setPosition(300, 300)
+
+  gameMap = sti("maps/".. mapName ..".lua")
+  for i, obj in pairs(gameMap.layers["Platforms"].objects) do
+    spawnPlatforms(obj.x, obj.y, obj.width, obj.height)
+  end
+
+  for i, obj in pairs(gameMap.layers["Enemies"].objects) do
+    spawnEnemy(obj.x, obj.y)
+  end
+
+  if gameMap.layers["NextLevel"] ~= nil then
+    for i, obj in pairs(gameMap.layers["NextLevel"].objects) do
+      flagX = obj.x
+      flagY = obj.y
     end
+  end
+end
+
+function destroyAll()
+  local i = #platforms
+  while i > -1 do
+    if platforms[i] ~= nil then
+      platforms[i]:destroy()
+    end
+    table.remove(platforms, i)
+    i = i - 1
+  end
+
+  local i = #enemies
+  while i > -1 do
+    if enemies[i] ~= nil then
+      enemies[i]:destroy()
+    end
+    table.remove(enemies, i)
+    i = i - 1
+  end
+end
+
+function spawnPlatforms(x, y, width, height)
+  if width > 0 and height > 0 then
+    platform = world:newRectangleCollider(x, y, width, height, { collision_class = 'Platform' })
+    platform:setType('static')
+    table.insert(platforms, platform)
   end
 end
---
+++ maps/level2.lua
@@ -0,0 +1,276 @@
+return {
+  version = "1.5",
+  luaversion = "5.1",
+  tiledversion = "1.7.2",
+  orientation = "orthogonal",
+  renderorder = "right-down",
+  width = 40,
+  height = 12,
+  tilewidth = 64,
+  tileheight = 64,
+  nextlayerid = 5,
+  nextobjectid = 20,
+  properties = {},
+  tilesets = {
+    {
+      name = "tileset",
+      firstgid = 1,
+      tilewidth = 64,
+      tileheight = 64,
+      spacing = 0,
+      margin = 0,
+      columns = 3,
+      image = "tileset.png",
+      imagewidth = 192,
+      imageheight = 192,
+      objectalignment = "unspecified",
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      grid = {
+        orientation = "orthogonal",
+        width = 64,
+        height = 64
+      },
+      properties = {},
+      wangsets = {},
+      tilecount = 9,
+      tiles = {}
+    },
+    {
+      name = "flag",
+      firstgid = 10,
+      tilewidth = 64,
+      tileheight = 64,
+      spacing = 0,
+      margin = 0,
+      columns = 1,
+      image = "flag.png",
+      imagewidth = 64,
+      imageheight = 64,
+      objectalignment = "unspecified",
+      tileoffset = {
+        x = 0,
+        y = 0
+      },
+      grid = {
+        orientation = "orthogonal",
+        width = 64,
+        height = 64
+      },
+      properties = {},
+      wangsets = {},
+      tilecount = 1,
+      tiles = {}
+    }
+  },
+  layers = {
+    {
+      type = "tilelayer",
+      x = 0,
+      y = 0,
+      width = 40,
+      height = 12,
+      id = 1,
+      name = "Camada de Tiles 1",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      properties = {},
+      encoding = "lua",
+      data = {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 7, 9, 4, 5, 5, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 5, 5, 6, 0, 0, 7, 8, 8, 8, 8, 9, 0, 7, 8, 8, 8, 8, 8, 8, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 7, 8, 8, 8, 9, 0, 0, 4, 5, 5, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 5, 5, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 8, 8, 8, 8, 8, 9, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 5, 5, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 5, 5, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 5, 5, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+      }
+    },
+    {
+      type = "objectgroup",
+      draworder = "topdown",
+      id = 2,
+      name = "Platforms",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      properties = {},
+      objects = {
+        {
+          id = 1,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 128,
+          y = 448,
+          width = 320,
+          height = 64,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 2,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 576,
+          y = 256,
+          width = 256,
+          height = 512,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 4,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 1984,
+          y = 512,
+          width = 448,
+          height = 64,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 10,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 960,
+          y = 384,
+          width = 384,
+          height = 64,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 11,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 1408,
+          y = 384,
+          width = 512,
+          height = 64,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 16,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 448,
+          y = 320,
+          width = 128,
+          height = 64,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        }
+      }
+    },
+    {
+      type = "objectgroup",
+      draworder = "topdown",
+      id = 4,
+      name = "NextLevel",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      properties = {},
+      objects = {
+        {
+          id = 14,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 2304,
+          y = 448,
+          width = 64,
+          height = 64,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 18,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 2304,
+          y = 448,
+          width = 64,
+          height = 64,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        }
+      }
+    },
+    {
+      type = "objectgroup",
+      draworder = "topdown",
+      id = 3,
+      name = "Enemies",
+      visible = true,
+      opacity = 1,
+      offsetx = 0,
+      offsety = 0,
+      parallaxx = 1,
+      parallaxy = 1,
+      properties = {},
+      objects = {
+        {
+          id = 5,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 1728,
+          y = 320,
+          width = 64,
+          height = 64,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        },
+        {
+          id = 6,
+          name = "",
+          type = "",
+          shape = "rectangle",
+          x = 1152,
+          y = 320,
+          width = 64,
+          height = 64,
+          rotation = 0,
+          visible = true,
+          properties = {}
+        }
+      }
+    }
+  }
+}
