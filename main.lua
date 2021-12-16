7b0dd7632c4ec87bd04ef82275ec6956a62915ad
Andre Leoni <andreluizleoni@gmail.com>
2021-12-16 12:34:14 -0200

    check collision with the ground

--- main.lua
+++ main.lua
@@ -44,7 +44,17 @@ end
 
 function love.keypressed(key)
   if key == 'up' then
-    player:applyLinearImpulse(0, -5000)
+    local colliders = world:queryRectangleArea(
+      player:getX() - 40,
+      player:getY() + 40,
+      80,
+      2,
+      {'Platform'}
+    )
+
+    if #colliders > 0 then
+      player:applyLinearImpulse(0, -5000)
+    end
   end
 end
 
