09ecb1ecdec4f9d0662b747071f9daa590c2df68
Andre Leoni <andreluizleoni@gmail.com>
2021-12-16 13:39:04 -0200

    add jump assets

--- main.lua
+++ main.lua
@@ -1,5 +1,22 @@
 function love.load()
   wf = require 'libraries/windfield/windfield'
+  anim8 = require 'libraries/anim8/anim8'
+
+  sprites = {}
+  sprites.playerSheet = love.graphics.newImage('sprites/playerSheet.png')
+
+  local grid = anim8.newGrid(
+    614,
+    564,
+    sprites.playerSheet:getWidth(),
+    sprites.playerSheet:getHeight()
+  )
+
+  animations = {}
+  animations.idle = anim8.newAnimation(grid('1-15', 1), 0.05)
+  animations.jump = anim8.newAnimation(grid('1-7', 2), 0.05)
+  animations.run = anim8.newAnimation(grid('1-15', 3), 0.05)
+
   world = wf.newWorld(0, 800, false)
   world:setQueryDebugDrawing(true)
 
@@ -7,9 +24,13 @@ function love.load()
   world:addCollisionClass('Platform')
   world:addCollisionClass('Danger')
 
-  player = world:newRectangleCollider(360, 100, 80, 80, { collision_class = 'Player' })
+  player = world:newRectangleCollider(360, 100, 40, 100, { collision_class = 'Player' })
   player:setFixedRotation(true)
   player.speed = 240
+  player.animation = animations.idle
+  player.isMoving = false
+  player.direction = 1
+  player.grounded = true
 
   platform = world:newRectangleCollider(250, 400, 300, 100, { collision_class = 'Platform' })
   platform:setType('static')
@@ -22,38 +43,65 @@ function love.update(dt)
   world:update(dt)
 
   if player.body then
-    local px, py = player:getPosition()
-
-    if love.keyboard.isDown('right') then
-      player:setX(px + player.speed * dt)
-    end
-
-    if love.keyboard.isDown('left') then
-      player:setX(px - player.speed * dt)
-    end
-  end
-
-  if player:enter('Danger') then
-    player:destroy()
-  end
-end
-
-function love.draw()
-  world:draw()
-end
-
-function love.keypressed(key)
-  if key == 'up' then
     local colliders = world:queryRectangleArea(
-      player:getX() - 40,
-      player:getY() + 40,
+      player:getX() - 20,
+      player:getY() + 50,
       80,
       2,
       {'Platform'}
     )
 
     if #colliders > 0 then
-      player:applyLinearImpulse(0, -5000)
+      player.grounded = true
+    else
+      player.grounded = false
+    end
+
+    player.isMoving = false
+
+    local px, py = player:getPosition()
+
+    if love.keyboard.isDown('right') then
+      player:setX(px + player.speed * dt)
+      player.isMoving = true
+      player.direction = 1
+    end
+
+    if love.keyboard.isDown('left') then
+      player:setX(px - player.speed * dt)
+      player.isMoving = true
+      player.direction = -1
+    end
+  end
+
+  if player.grounded then
+    if player.isMoving then
+      player.animation = animations.run
+    else
+      player.animation = animations.idle
+    end
+  else
+    player.animation = animations.jump
+  end
+
+  if player:enter('Danger') then
+    player:destroy()
+  end
+
+  player.animation:update(dt)
+end
+
+function love.draw()
+  world:draw()
+
+  local px, py = player:getPosition()
+  player.animation:draw(sprites.playerSheet, px, py, nil, 0.25 * player.direction, 0.25, 130, 300)
+end
+
+function love.keypressed(key)
+  if key == 'up' then
+    if player.grounded then
+      player:applyLinearImpulse(0, -4000)
     end
   end
 end
